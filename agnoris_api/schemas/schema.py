7d9c4656512fb3a2b88b7c58d9aece6bcd921edb
Alex <matse004@umn.edu>
2018-03-29 09:16:07 +0300

    commented out all shrmas but daily_rds

--- agnoris_api/schemas/schema.py
+++ agnoris_api/schemas/schema.py
@@ -1,10 +1,10 @@
 import graphene
 
-from agnoris_api.schemas import checks, reservations, vanity, labor, check_items, reports, daily_report_rds
+# checks, reservations, vanity, labor, check_items, reports,
+from agnoris_api.schemas import daily_report_rds
 
-
-class Query(checks.Query, reservations.Query, vanity.Query, labor.Query, check_items.Query, reports.Query,
-            daily_report_rds.Query, graphene.ObjectType):
+# checks.Query, reservations.Query, vanity.Query, labor.Query, check_items.Query, reports.Query,
+class Query(daily_report_rds.Query, graphene.ObjectType):
     pass
 
 
